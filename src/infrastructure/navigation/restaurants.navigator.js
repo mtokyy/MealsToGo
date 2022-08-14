63a9169408099e788d0fa7d58568a116b4ee6f69
mtokyy <mtokyy@gmail.com>
2022-08-14 14:36:57 +0800

    add restaurant detail screen

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,17 +1,28 @@
 import React from "react";
-
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
-
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator screenOptions={{ headerShown: false }}>
+    <RestaurantStack.Navigator
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+        headerShown: false,
+      }}
+    >
       <RestaurantStack.Screen
-        name="RestaurantsStackScreen"
+        name="RestaurantsScreen"
         component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
