80abfb8ebe898117713606dd1e6aeb060003b304
mtokyy <mtokyy@gmail.com>
2022-08-14 22:04:07 +0800

    add account navigator

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppNavigator } from "./app.navigator";
+import { AccountNavigator } from "./account.navigator";
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { NavigationContainer } from "@react-navigation/native";
+import { View, Text } from "react-native";
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  // const { isAuthenticated } = useContext(AuthenticationContext);
+
+  const { isAuthenticated } = useContext(AuthenticationContext);
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
