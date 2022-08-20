66a569bd15635df88797b4fc6d0fba4766ea1265
mtokyy <mtokyy@gmail.com>
2022-08-20 11:59:05 +0800

    infrastructure

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -3,7 +3,6 @@ import { AppNavigator } from "./app.navigator";
 import { AccountNavigator } from "./account.navigator";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
 import { NavigationContainer } from "@react-navigation/native";
-import { View, Text } from "react-native";
 
 export const Navigation = () => {
   // const { isAuthenticated } = useContext(AuthenticationContext);
